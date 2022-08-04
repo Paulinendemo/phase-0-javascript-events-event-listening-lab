c2456ecc884ab2d363d837bc2c382230015eb50b
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-08-04 09:01:46 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
+
+
 function addingEventListener() {
+    let input;
+    input = document.getElementById('button')
+    input.addEventListener('click', () => {
+        alert('button clicked');
+    })
 }
+addingEventListener()
